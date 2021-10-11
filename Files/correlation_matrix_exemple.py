71e6fade82557a69db9a10fdde74586249056ff5
Aphonso Henrique do Amaral Rafael <56922195+aphonsoar@users.noreply.github.com>
2021-10-11 17:39:47 +0100

    Update correlation matrix example.

--- Files/correlation_matrix_exemple.py
+++ Files/correlation_matrix_exemple.py
@@ -14,18 +14,29 @@ data_corrMatrix = data1[[
                'SIMPLES', 'INSS_SIMPLES', 'IRRF', 'INSS_PRESUMIDO', 'COFINS', 'PIS', 'ISS', 'FGTS']]
 corrMatrix = data_corrMatrix.corr()
 
+# Filter the dataframe with correlations greater or smaller than the threshold value: https://stackoverflow.com/questions/17778394/list-highest-correlation-pairs-from-a-large-correlation-matrix-in-pandas
+threshold_matrix = 0.5
+filteredDf = corrMatrix[((corrMatrix >= threshold_matrix) | (corrMatrix <= -threshold_matrix)) & (corrMatrix !=1.000)]
+filteredDf_dropNA = filteredDf.dropna(axis = 0, how = 'all') # Keep just the rows greater than the trheshold
+filteredDf_dropNA = filteredDf_dropNA.dropna(axis = 1, how = 'all') # Keep just the columns greater than the trheshold
+
+#%%
+# Select which matrix should be used to plot: full, filtered or filtered and drop the columns
+matrix_used = filteredDf_dropNA # corrMatrix / filteredDf
+figsize = 10
+
 # Using just the bottom diagonal from the matrix:
 #mask = np.zeros_like(corrMatrix)
 #mask[np.triu_indices_from(mask)] = True
 
 # Using just the upper diagonal from the matrix:
-mask = np.ones_like(corrMatrix)
+mask = np.ones_like(matrix_used)
 mask[np.triu_indices_from(arr=mask, k=+1)] = False
 
 # Plotar matriz de correlação:
 with sns.axes_style("white"):
-    f, ax = plt.subplots(figsize=(8, 8))
-    ax = sns.heatmap(data=corrMatrix,
+    f, ax = plt.subplots(figsize=(figsize, figsize))
+    ax = sns.heatmap(data=matrix_used,
                      annot=True,
                      fmt='.2f',
                      cmap= 'bwr', #'twilight_shifted', # Colormaps: https://matplotlib.org/stable/tutorials/colors/colormaps.html
@@ -40,8 +51,8 @@ with sns.axes_style("white"):
     #ax.set(title='Análise binária: Ter tickets / ter tickets de impostos / ter guias dos impostos')
     plt.xticks(rotation=90)
     plt.tight_layout()
-    plt.savefig('C:\\Aphonso\\BI\\business_intelligence\\data_science\\aphonso\\Engajamento\\Contact_rate_impostos_mai21\\'+
-                title + '_binaria_'+periodo_analise_tickets+'.png')
+    #plt.savefig('C:\\Aphonso\\BI\\business_intelligence\\data_science\\aphonso\\Engajamento\\Contact_rate_impostos_mai21\\'+
+    #            title + '_binaria_'+periodo_analise_tickets+'.png')
     plt.show()
     # Gif maker: https://ezgif.com/maker
 
